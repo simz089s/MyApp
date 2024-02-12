b19931d93f8900664cc953ec64edf6913c46b6a0
Simon Zheng <simz97@outlook.com>
2024-02-12 06:55:58 -0500

    Initial commit v4

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,3 +4,23 @@ describe('My First Test', () => {
     cy.contains('app is running!')
   })
 })
+
+describe('myapp app', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('should display correct title', () => {
+    cy.get('h1').first().should('have.text', 'Default');
+  });
+
+  afterEach(() => {
+    // Assert that there are no errors emitted from the browser
+    cy.window().then((win) => {
+      const logs = win.console.logs();
+      expect(logs).not.to.contain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
+    });
+  });
+});
